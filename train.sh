47dea5ed2f2a64fef5c1dcdbbdf1363428391f64
Jeremy Carleton <jcarleton_tamu.edu@slogin-01.cm.cluster>
2026-05-01 11:35:58 -0500

    Reduce max_actor_ckpt_to_keep

--- train.sh
+++ train.sh
@@ -239,7 +239,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.nnodes=1 \
     trainer.save_freq=50 \
     trainer.test_freq=10 \
-    trainer.max_actor_ckpt_to_keep=3 \
+    trainer.max_actor_ckpt_to_keep=2 \
     +actor_rollout_ref.actor.checkpoint.save_contents=["hf_model","extra"] \
     trainer.total_epochs=1 \
     trainer.default_local_dir=${SAVE_PATH} \
